7b4400aa7b56abbf0c9028bb2a7195bd717a846d
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-22 21:37:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,10 +3,6 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Welcome to your app!!!! "
-  end
-
-  get '/' do
     "Welcome to your app!!!! I BUILT THIS!"
   end
 
